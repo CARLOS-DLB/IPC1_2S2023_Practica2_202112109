9f255b7f939aa8484b14a959c7b67a7d49bb5992
CARLOS-DLB <deleonbarrioscd@gmail.com>
2023-10-01 20:21:05 -0600

    Inicio Proyecto

---
+++ src/main/java/com/mycompany/practica2/Main.java
@@ -0,0 +1,10 @@
+package com.mycompany.practica2;
+
+import GUI.Delivery;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Delivery delivery = new Delivery();
+    }
+}
